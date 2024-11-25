eb9a302007527c2dfcdfa175b871e2ac4848ba2c
Anirudh Chhabra <anirudhchhabra96@gmail.com>
2024-11-25 18:11:10 -0500

    ..

--- pi_hexapod_driver/pi_hexapod_control/src/velocity_control_node.py
+++ pi_hexapod_driver/pi_hexapod_control/src/velocity_control_node.py
@@ -7,10 +7,17 @@ import numpy as np
 import math
 import time
 import warnings
+from std_msgs.msg import Int32MultiArray
 
 class HexTrajectoryControl(object):
     def __init__(self):
         rospy.init_node("hex_trajectory_control")
+        self.trajectory_list = [
+           [0.0, 0.0, 0, 0, 0.0, 0.0] for i in range(1)
+        ]
+        self.how_long = 0.0
+
+        self.get_vel_cmd = rospy.Subscriber('/target_vel_hex', Float64MultiArray ,self.get_vel_cmd_callback)
 
         self.qdot_pub = rospy.Publisher(
                         '/pi_hardware_interface/qdot_cmd',
@@ -19,6 +26,14 @@ class HexTrajectoryControl(object):
         )
 
         # Each qdot trajectory point will execute for this time (in ms).
+
+    def get_vel_cmd_callback(self, msg):
+        self.trajectory_list = [
+           msg.data[:5] for i in range(1)
+           
+        ]
+        # print(msg.data)
+        self.how_long = msg.data[-1]*1000
 
     def publish(self, trajectory: list, qdot_period:float=50):
         """
@@ -52,17 +67,17 @@ class HexTrajectoryControl(object):
 
 
     def run(self):
-        trajectory = [
-           [0.0, 0.0, -50, 0.0, 0.0, 0.0] for i in range(1)
-        ]
+        # trajectory = [
+        #    [-20.0, 0.0, 0, 0, 0.0, 0.0] for i in range(1)
+        # ]
 
         #while not rospy.is_shutdown():
         for i in range(2):
-            self.publish(trajectory, 500)
+            # howlong = 4*1000
+            self.publish(self.trajectory_list, self.how_long)
             rospy.sleep(1)
-
-        
 
 if __name__ == "__main__":
     node = HexTrajectoryControl()
+    # while not rospy.is_shutdown():
     node.run()
