ba78668278a43a6e76f9de581cdb4d93017284ba
Anirudh Chhabra <anirudhchhabra96@gmail.com>
2025-02-25 21:30:23 -0500

    ..

--- pi_hexapod_driver/pi_hexapod_control/src/velocity_control_node.py
+++ pi_hexapod_driver/pi_hexapod_control/src/velocity_control_node.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rospy
 
 from std_msgs.msg import Float64MultiArray
@@ -9,8 +9,6 @@ import time
 import warnings
 from sensor_msgs.msg import JointState
 import sys
-
-
 
 class HexTrajectoryControl(object):
     def __init__(self):
